dab1ce9096fbf5c2256659831d17dc04c1306148
Max Tyler <maxastyler@gmail.com>
2019-11-03 14:25:11 +0000

    Finish stuff

--- 2016/day_4/day_4.clj
+++ 2016/day_4/day_4.clj
@@ -1,16 +1,69 @@
 (require '[clojure.string :as str])
 
-(defn convert-num-str [t]
-  (->> (str/split t #" ")
-       (filter #(not= "" %))
-       (map #(Integer/parseInt %))))
 (defn remove-dashes [s]
   (apply str (str/split s #"-")))
+
+(defn group-by-frequency [s]
+  (let [f-dict (frequencies s)]
+    (group-by #(get f-dict %) (keys f-dict))))
+
+(defn shift-char [c s]
+  (char (+
+         (mod (+
+               (- (int c)
+                  (int \a))
+               s) 26)
+         (int \a))))
+
+(defn shift-name [n s]
+  (defn shift-or-replace [c]
+    (case c
+      \- \space
+      (shift-char c s)
+      ))
+  (apply str (map shift-or-replace n))
+  )
+
+(defn freq-dict-to-vec [f]
+  (->> (keys f)
+       (sort >)
+       (map #(->> (get f %)
+                  (apply str)
+                  (sort)
+              ))
+       flatten
+       (take 5)
+       (apply str)))
 
 (def input
   (->> (str/split (slurp "./input") #"\n")
        (map #(re-find #"(\S+\-)(\d+)\[(\S+)\]" %))
        (map (fn [[_ name id csum]]
               {:name (remove-dashes name)
-               :id id
+               :id (Integer/parseInt id)
                :csum csum}))))
+
+(def input
+  (->> (str/split (slurp "./input") #"\n")
+       (map #(re-find #"(\S+\-)(\d+)\[(\S+)\]" %))
+       (map (fn [[_ name id csum]]
+              {:name (remove-dashes name)
+               :id (Integer/parseInt id)
+               :csum csum}))))
+
+(def part2-input
+  (->> (str/split (slurp "./input") #"\n")
+       (map #(re-find #"(\S+\-)(\d+)\[(\S+)\]" %))
+       (map (fn [[_ name id csum]]
+              {:name (shift-name name (Integer/parseInt id))
+               :id (Integer/parseInt id)
+               :csum csum}))))
+
+(def valid-inputs
+  (filter
+   #(= (:csum %)
+       (freq-dict-to-vec
+        (group-by-frequency (:name %)))) input))
+
+(def valid-inputs-sum
+  (reduce + 0 (map :id valid-inputs)))
