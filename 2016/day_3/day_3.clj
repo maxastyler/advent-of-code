1b1cefb2a13e4597036f6ac3f974a73e4d85b80e
Max Tyler <maxastyler@gmail.com>
2019-11-03 12:35:23 +0000

    Add clojure solution for day 3

---
+++ 2016/day_3/day_3.clj
@@ -0,0 +1,28 @@
+(require '[clojure.string :as str])
+
+(defn convert-num-str [t]
+  (->> (str/split t #" ")
+       (filter #(not= "" %))
+       (map #(Integer/parseInt %))))
+
+(def input
+  (->> (str/split (slurp "./input") #"\n")
+       (map convert-num-str)))
+
+(def trans-input
+  (->> input
+       transpose
+       flatten
+       (partition 3)))
+
+(def valid-triangles (filter valid-triangle input))
+(def valid-trans-triangles (filter valid-triangle trans-input))
+
+(defn transpose [a]
+  (apply mapv vector a))
+
+(defn valid-triangle [[a b c]]
+  (and
+   (> (+ a b) c)
+   (> (+ b c) a)
+   (> (+ c a) b)))
