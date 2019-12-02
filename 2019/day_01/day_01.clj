b3ca600cf5e674ce77d80a70c3e34a3da9ed604f
Max Tyler <maxastyler@gmail.com>
2019-12-02 09:50:34 +0000

    Add part 2

--- 2019/day_01/day_01.clj
+++ 2019/day_01/day_01.clj
@@ -5,8 +5,8 @@
 (defn fuel-req [x] (- (quot x 3) 2))
 
 (def part-1 (transduce (map fuel-req) + input))
-(def part-2-func (transduce (map #(reduce + 0
-                                          (rest
-                                           (take-while pos?
-                                                       (iterate fuel-req %)))))
-                            + input))
+(def part-2 (transduce (map #(reduce +
+                                     (rest (take-while
+                                            pos?
+                                            (iterate fuel-req %)))))
+                       + input))
