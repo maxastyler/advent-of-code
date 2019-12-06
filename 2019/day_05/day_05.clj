78d955642c9ccd860bfadc1b60fec7e44e784c68
Max Tyler <maxastyler@gmail.com>
2019-12-06 17:49:47 +0000

    Finish computer for day 5

--- 2019/day_05/day_05.clj
+++ 2019/day_05/day_05.clj
@@ -4,9 +4,6 @@
              (map read-string i)
              (vec i)))
 
-(defn exp [x n]
-  (reduce * (repeat n x)))
-
 (defn intcode
   "takes in a dictionary of {:pointer pointer :tape tape
                              :input input :output output} and returns
@@ -16,6 +13,7 @@
     state
     (let [instruction (tape pointer)
           opcode (mod instruction 100)
+          exp (fn [x n] (reduce * (repeat n x)))
           p-mode #(mod (quot instruction (exp 10 (+ % 2))) 10)
           n #(case (p-mode %)
                0 (tape (tape (+ pointer % 1)))
@@ -35,11 +33,34 @@
         4 (assoc state
                  :pointer (+ 2 pointer)
                  :output (conj output (n 0)))
+        5 (assoc state
+                 :pointer (if (not= 0 (n 0))
+                            (n 1)
+                            (+ 3 pointer)))
+        6 (assoc state
+                 :pointer (if (= 0 (n 0))
+                            (n 1)
+                            (+ 3 pointer)))
+        7 (assoc state
+                 :pointer (+ 4 pointer)
+                 :tape (assoc tape (w 2) (if (< (n 0) (n 1))
+                                           1
+                                           0)))
+        8 (assoc state
+                 :pointer (+ 4 pointer)
+                 :tape (assoc tape (w 2) (if (= (n 0) (n 1))
+                                           1
+                                           0)))
         (reduced state)))))
 
-(def part-1 (->> {:pointer 0 :tape input :input [1] :output []}
-                 (iterate intcode)
-                 (take-while #(not (reduced? %)))
-                 (last)
+(defn run-until-reduced [tape input] (->> (iterate intcode {:pointer 0 :tape tape :input input :output []})
+                                     (take-while #(not (reduced? %)))
+                                     (last)))
+
+(def part-1 (->> (run-until-reduced input [1])
+                 (:output)
+                 (peek)))
+
+(def part-2 (->> (run-until-reduced input [5])
                  (:output)
                  (peek)))
