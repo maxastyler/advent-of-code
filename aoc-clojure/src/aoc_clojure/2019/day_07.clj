3c570e290cc5feac0d95b5d79ee944a187db37e8
Max Tyler <maxastyler@gmail.com>
2019-12-08 16:16:32 +0000

    Add rust day 7

--- aoc-clojure/src/aoc_clojure/2019/day_07.clj
+++ aoc-clojure/src/aoc_clojure/2019/day_07.clj
@@ -7,3 +7,36 @@
             (clojure.string/trim-newline i)
             (clojure.string/split i #",")
             (mapv read-string i)))
+
+(def initial-states
+  (update-in (vec
+              (repeat 4 {:pointer 0 :tape prog
+                         :input (conj (clojure.lang.PersistentQueue/EMPTY) 8)}))
+             [0 :input] #(conj % 0)))
+
+(defn one-round
+  "run one round of computations"
+  [states]
+  (loop [i 0 s states]
+    (let [ns (run-until-paused (s i))]
+      (if (>= i (dec (count states)))
+        (assoc-in s [i] ns)
+        (recur (inc i)
+               (-> s
+                   (assoc-in [i] ns)
+                   (update-in [(inc i) :input] #(apply conj % (:output ns)))
+                   (assoc-in [i :output] (clojure.lang.PersistentQueue/EMPTY))))))))
+
+(defn recursive-machine [phases]
+  (let [s (mapv #(hash-map :pointer 0 :tape prog
+                           :input (conj (clojure.lang.PersistentQueue/EMPTY) %)
+                           :output (clojure.lang.PersistentQueue/EMPTY)) phases)]
+    (loop [ns (one-round s)]
+      (if (:finished (ns (dec (count phases))))
+        ns
+        (recur
+         (one-round
+          (-> ns
+              (update-in [0 :input] #(apply conj % (:output (peek ns))))
+              (assoc-in [(dec (count ns)) :output]
+                        (clojure.lang.PersistentQueue/EMPTY)))))))))
--- aoc-clojure/src/aoc_clojure/2019/intcode.clj
+++ aoc-clojure/src/aoc_clojure/2019/intcode.clj
@@ -6,6 +6,69 @@
             [clojure.test :as t]))
 
 (defn intcode
+  "takes in a dictionary which should contain at least :pointer and :tape"
+  [{:keys [pointer tape input output] :as state}]
+  (let [instruction (tape pointer)
+        opcode (mod instruction 100)
+        exp (fn [x n] (reduce * (repeat n x)))
+        p-mode #(mod (quot instruction (exp 10 (+ % 2))) 10)
+        n #(case (p-mode %)
+             0 (tape (tape (+ pointer % 1)))
+             1 (tape (+ pointer % 1)))
+        w #(tape (+ pointer % 1))]
+    (case opcode 
+      1 (assoc state
+               :pointer (+ 4 pointer)
+               :tape (assoc tape (w 2) (+ (n 0) (n 1))))
+      2 (assoc state
+               :pointer (+ 4 pointer)
+               :tape (assoc tape (w 2) (* (n 0) (n 1))))
+      3 (if-let [i (peek input)]
+          (assoc state
+                 :pointer (+ 2 pointer)
+                 :waiting nil
+                 :input (pop input)
+                 :tape (assoc tape (w 0) i))
+          (assoc state :waiting true))
+      4 (assoc state
+               :pointer (+ 2 pointer)
+               :output (conj output (n 0)))
+      5 (assoc state
+               :pointer (if-not (zero? (n 0))
+                          (n 1)
+                          (+ 3 pointer)))
+      6 (assoc state
+               :pointer (if (zero? (n 0))
+                          (n 1)
+                          (+ 3 pointer)))
+      7 (assoc state
+               :pointer (+ 4 pointer)
+               :tape (assoc tape (w 2) (if (< (n 0) (n 1))
+                                         1
+                                         0)))
+      8 (assoc state
+               :pointer (+ 4 pointer)
+               :tape (assoc tape (w 2) (if (= (n 0) (n 1))
+                                         1
+                                         0)))
+      99 (assoc state :finished true))))
+
+(t/with-test 
+  (defn run-until-paused [s]
+    "run the intcode computer on s until waiting or finished"
+    (loop [current-state s]
+      (let [ns (intcode current-state)]
+        (if (or (:waiting ns)
+                (:finished ns))
+          ns
+          (recur ns))))))
+
+  (t/is (= (peek ((run-until-paused 0 [3,9,8,9,10,9,4,9,99,-1,8] [8]) 1)) 1))
+  (t/is (= (peek ((run-until-paused 0 [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9] [8]) 1)) 1))
+  (t/is (= (peek ((run-until-paused 0 [3,3,1105,-1,9,1101,0,0,12,4,12,99,1] [8]) 1)) 1))
+  (t/is (= (peek ((run-until-paused 0 [3,9,7,9,10,9,4,9,99,-1,8] [5]) 1)) 1)))
+
+(defn intcode*
   "takes in a dictionary of {:pointer pointer :tape tape
                              :input input :output output} and returns
   a new state of the computer"
@@ -55,15 +118,16 @@
                                          0)))
       99 (assoc state :finished true))))
 
+
 (t/with-test 
-  (defn run-until-paused [pointer tape inputs]
+  (defn run-until-paused* [pointer tape inputs]
     "Return a vector contatining the final state and a queue of outputs [s o]"
     (loop [p pointer
            t tape
            i inputs
            o (clojure.lang.PersistentQueue/EMPTY)]
       (let [{:keys [pointer tape input-taken finished waiting output] :as s}
-            (intcode p t (peek i))]
+            (intcode* p t (peek i))]
         (if (or finished waiting)
           [s o]
           (recur pointer tape
