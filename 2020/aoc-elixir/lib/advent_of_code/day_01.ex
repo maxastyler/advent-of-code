a26c0a1006cac19710c1e7acfdf226b2f9540ac0
Max Tyler <maxastyler@gmail.com>
2020-12-01 12:06:00 +0000

    Do the elixir solution

--- 2020/aoc-elixir/lib/advent_of_code/day_01.ex
+++ 2020/aoc-elixir/lib/advent_of_code/day_01.ex
@@ -1,7 +1,23 @@
 defmodule AdventOfCode.Day01 do
+
+  def format_input(inp) do
+    inp
+    |> String.trim()
+    |> String.split()
+    |> Enum.map(&(String.to_integer(&1)))
+  end
+
   def part1(args) do
+    inp = format_input(args)
+    for x <- inp, y <- inp, x + y == 2020 do
+                                x * y
+    end |> Enum.take(1)
   end
 
   def part2(args) do
+    inp = format_input(args)
+    for x <- inp, y <- inp, z <- inp, x + y + z == 2020 do
+                                              x * y * z
+    end |> Enum.take(1)
   end
 end
