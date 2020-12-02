247b2bccfc6a4509b8f90095a5036f22f90db0a4
Max Tyler <maxastyler@gmail.com>
2020-12-02 08:19:58 +0000

    Complete day 2 clojure

---
+++ 2020/aoc-clojure-2020/src/aoc_clojure_2020/day-02.clj
@@ -0,0 +1,25 @@
+(ns aoc-clojure-2020.day-02
+  (:require [aoc-clojure-2020.helpers :refer [get-input]]))
+
+(def input (->> (get-input 2)
+                (re-seq #"(\d+)-(\d+)\ ([a-z]):\ ([a-z]+)")
+                (map (fn [[_ mi ma ch pass]] [(Integer/parseInt mi)
+                                              (Integer/parseInt ma)
+                                              (first (char-array ch))
+                                              pass]))))
+
+(defn pass-valid-1? [[mi, ma, letter, password]]
+  (let [letter-count (->> password
+                          (filter #(= %1 letter))
+                          count)]
+    (and (>= letter-count mi)
+         (<= letter-count ma))))
+
+(defn pass-valid-2? [[mi, ma, letter, password]]
+  (let [s1 (= (get password (dec mi)) letter)
+        s2 (= (get password (dec ma)) letter)]
+    (and (or s1 s2) (not= s1 s2))))
+
+
+(def part-1 (count (filter pass-valid-1? input)))
+(def part-2 (count (filter pass-valid-2? input)))
