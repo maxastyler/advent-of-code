931273057c4dae5bce3e7f6c639cd1391ca844b0
Max Tyler <maxastyler@gmail.com>
2020-12-03 09:05:33 +0000

    Add elixir solution

--- 2020/aoc-clojure-2020/src/aoc_clojure_2020/day-03.clj
+++ 2020/aoc-clojure-2020/src/aoc_clojure_2020/day-03.clj
@@ -7,25 +7,13 @@
 
 (defn gradient-tree-count [tree-map x-step y-step]
   (let [width (count (first tree-map))
-        positions (iterate (fn [[x y]] [(mod (+ x x-step) width) (+ y y-step)]) [0 0])]
-    (->> (map #(get-in tree-map (reverse %)) positions)
-         (take-while some?)
+        x-positions (iterate #(mod (+ % x-step) width) 0)]
+    (->> (take-nth y-step tree-map)
+         (map #(get %2 %1) x-positions)
          (filter #(= % \#))
          count)))
-
-(defn gradient-tree-count-loop [tree-map x-step y-step]
-  (let [width (count (first tree-map))
-        height (count tree-map)
-        tree-count-at-space (fn [x y] (if (= (get-in tree-map [y x]) \#) 1 0))]
-    (loop [x 0 y 0 trees 0]
-      (if (< y height)
-        (recur (mod (+ x x-step) width) (+ y y-step) (+ trees (tree-count-at-space x y)))
-        trees))))
 
 (def part-1 (gradient-tree-count input 3 1))
 (def part-2 (->> (map #(apply gradient-tree-count input %)
                       [[1 1] [3 1] [5 1] [7 1] [1 2]])
                  (apply *)))
-
-(println "Part 1: " part-1)
-(println "Part 2: " part-2)
--- 2020/aoc-elixir/lib/advent_of_code/day_03.ex
+++ 2020/aoc-elixir/lib/advent_of_code/day_03.ex
@@ -1,7 +1,22 @@
 defmodule AdventOfCode.Day03 do
+
+  def toboggan_race(step_x, step_y, [first_line | _] = trees) do
+    width = String.length(first_line)
+    x_positions = Stream.iterate(0, fn x -> rem(x + step_x, width) end)
+    Stream.take_every(trees, step_y)
+    |> Stream.zip(x_positions)
+    |> Stream.map(fn {line, x} -> if String.at(line, x) == "#", do: 1, else: 0 end)
+    |> Enum.sum()
+  end
+
   def part1(args) do
+    toboggan_race(3, 1, String.split(args, "\n"))
   end
 
   def part2(args) do
+    trees = String.split(args, "\n")
+    [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+    |> Enum.map(fn [x_step, y_step] -> toboggan_race(x_step, y_step, trees) end)
+    |> Enum.reduce(1, fn (e, a) -> e * a end)
   end
 end
