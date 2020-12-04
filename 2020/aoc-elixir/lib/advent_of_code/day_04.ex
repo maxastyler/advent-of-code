4ac8b4dd0f8478f4e0b9ee294bce39fa3426f079
Max Tyler <maxastyler@gmail.com>
2020-12-04 13:19:04 +0000

    Update to use ranges

--- 2020/aoc-elixir/lib/advent_of_code/day_04.ex
+++ 2020/aoc-elixir/lib/advent_of_code/day_04.ex
@@ -14,14 +14,14 @@ defmodule AdventOfCode.Day04 do
     Enum.filter(input_to_maps(args), &has_keys/1) |> Enum.count
   end
 
-  def val_byr(x), do: (1920 <= String.to_integer(x)) and (String.to_integer(x) <= 2002)
-  def val_iyr(x), do: (2010 <= String.to_integer(x)) and (String.to_integer(x) <= 2020)
-  def val_eyr(x), do: (2020 <= String.to_integer(x)) and (String.to_integer(x) <= 2030)
-  def val_hgt([_, x, "cm"]), do: (150 <= String.to_integer(x)) and (String.to_integer(x) <= 193)
-  def val_hgt([_, x, "in"]), do: (59 <= String.to_integer(x)) and (String.to_integer(x) <= 76)
+  def val_byr(x), do: String.to_integer(x) in 1920..2002
+  def val_iyr(x), do: String.to_integer(x) in 2010..2020
+  def val_eyr(x), do: String.to_integer(x) in 2020..2030
+  def val_hgt([_, x, "cm"]), do: String.to_integer(x) in 150..193
+  def val_hgt([_, x, "in"]), do: String.to_integer(x) in 59..76
   def val_hgt(_), do: false
   def val_hcl(x), do: Regex.match?(~r/#[0-9a-f]{6}/, x)
-  def val_ecl(x), do: Enum.member?(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"], x)
+  def val_ecl(x), do: x in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
   def val_pid(x), do: Regex.match?(~r/^\d{9}$/, x)
 
   def val_passport(%{byr: byr, iyr: iyr, eyr: eyr, hgt: hgt, hcl: hcl, ecl: ecl, pid: pid}) do
