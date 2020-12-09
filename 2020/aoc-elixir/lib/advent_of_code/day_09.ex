5f9c9107dfb2a7b3732d1b567c833656a6e2342a
Max Tyler <maxastyler@gmail.com>
2020-12-09 23:50:13 +0000

    Add elixir day 9 code

--- 2020/aoc-elixir/lib/advent_of_code/day_09.ex
+++ 2020/aoc-elixir/lib/advent_of_code/day_09.ex
@@ -1,7 +1,28 @@
 defmodule AdventOfCode.Day09 do
+  def parse_input(input) do
+    String.trim(input)
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def find_bad_encoding(nums) do
+    Stream.chunk_every(nums, 26, 1)
+    |> Stream.reject(&(List.last(&1) in (for {x, i} <- Enum.with_index(Enum.take(&1, 25)),
+                                             y <- Enum.drop(&1, i) |> Enum.take(25), do: x + y)))
+    |> Enum.take(1) |> List.first |> List.last
+  end
+
   def part1(args) do
+    parse_input(args)
+    |> find_bad_encoding
   end
 
   def part2(args) do
+    input = parse_input(args)
+    bad_num = find_bad_encoding(input)
+    Stream.map(2..Enum.count(input), &(Stream.chunk_every(input, &1, 1)))
+    |> Stream.concat
+    |> Stream.filter(&(Enum.sum(&1) == bad_num))
+    |> Enum.take(1) |> List.first |> (&(Enum.min(&1) + Enum.max(&1))).()
   end
 end
