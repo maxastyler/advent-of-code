49d6a2df14c804054beaed3b09e96961fbf3bfd4
Max Tyler <maxastyler@gmail.com>
2020-12-10 14:38:47 +0000

    Do clojure day 10

--- 2020/aoc-clojure-2020/src/aoc_clojure_2020/day_10.clj
+++ 2020/aoc-clojure-2020/src/aoc_clojure_2020/day_10.clj
@@ -1,1 +1,28 @@
-(ns aoc-clojure-2020.day-10)
+(ns aoc-clojure-2020.day-10
+  (:require [aoc-clojure-2020.helpers :refer [get-input]]))
+
+(def input (->> (get-input 10)
+                clojure.string/split-lines
+                (map read-string)
+                sort
+                (#(concat [0] % [(+ 3 (last %))]))
+                (into [])))
+
+(def part-1 (->> input
+                 (partition 2 1)
+                 (map #(apply - (reverse %)))
+                 (frequencies)
+                 ((fn [{ones 1 threes 3}] (* ones threes)))))
+
+(def inp (mapv vector (range) input))
+
+(def count-paths (memoize
+                  (fn [[i v]]
+                    (if (>= i (dec (count inp)))
+                      1
+                      (->> (drop (inc i) inp)
+                           (take-while #(<= (- (second %) v) 3))
+                           (map count-paths)
+                           (apply +))))))
+
+(def part-2 (count-paths [0 0]))
