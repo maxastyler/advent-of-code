d02972cfe80f6b92e91703d526309ab92e3b7aed
Max Tyler <maxastyler@gmail.com>
2020-12-15 10:02:43 +0000

    Don't reduce - use into lol

--- 2020/aoc-clojure-2020/src/aoc_clojure_2020/day_15.clj
+++ 2020/aoc-clojure-2020/src/aoc_clojure_2020/day_15.clj
@@ -2,7 +2,6 @@
 
 (defn take-turn [[[val t] turns]]
   [[(if-let [t' (turns val)] (- t t') 0) (inc t)] (assoc turns val t)])
-(defn game-start [nums] (reduce (fn [[_ t] [i v]] [[v i] (assoc t v i)])
-                                [[0 0] {}] (->> nums (map vector (range)))))
+(defn game-start [nums] [[(last nums) (dec (count nums))] (into {} (map vector nums (range)))])
 (-> (iterate take-turn (game-start [18 11 9 0 5 1])) (nth (- 2020 6)) first first)
 (-> (iterate take-turn (game-start [18 11 9 0 5 1])) (nth (- 30000000 6)) first first)
