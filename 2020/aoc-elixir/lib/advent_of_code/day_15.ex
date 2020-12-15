54b84b5c9ec0bb0c2253e231678224c9872cb7a6
Max Tyler <maxastyler@gmail.com>
2020-12-15 11:26:46 +0000

    Clean up

--- 2020/aoc-elixir/lib/advent_of_code/day_15.ex
+++ 2020/aoc-elixir/lib/advent_of_code/day_15.ex
@@ -1,19 +1,20 @@
 defmodule AdventOfCode.Day15 do
   def take_turn({{value, turn}, last_times}) do
-    case Map.get(last_times, value) do
-      nil -> {{0, turn + 1}, Map.put(last_times, value, turn)}
-      spoken_when -> {{turn - spoken_when, turn + 1}, Map.put(last_times, value, turn)}
-    end
+    new_value = case Map.get(last_times, value), do: (nil -> 0; x -> turn - x)
+    {{new_value, turn + 1}, Map.put(last_times, value, turn)}
   end
 
   def get_nth_turn(starter_nums, nth) do
-    turns = Enum.with_index(starter_nums) |> Enum.reverse
+    turns = Enum.with_index(starter_nums) |> Enum.reverse()
     last_times = Map.new(turns)
-    {{n, _}, _} = Stream.iterate({List.first(turns), last_times}, &take_turn/1)
-    |> Enum.at(nth - Enum.count(turns))
+
+    {{n, _}, _} =
+      Stream.iterate({List.first(turns), last_times}, &take_turn/1)
+      |> Enum.at(nth - Enum.count(turns))
+
     n
   end
 
   def part1(args), do: get_nth_turn(args, 2020)
-  def part2(args), do: get_nth_turn(args, 30000000)
+  def part2(args), do: get_nth_turn(args, 30_000_000)
 end
