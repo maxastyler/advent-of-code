073e7fd8e521c3e48a93173d70b9e64a2094d690
Max Tyler <maxastyler@gmail.com>
2020-12-15 10:10:14 +0000

    Neaten up

--- 2020/aoc-elixir/lib/advent_of_code/day_15.ex
+++ 2020/aoc-elixir/lib/advent_of_code/day_15.ex
@@ -1,18 +1,14 @@
 defmodule AdventOfCode.Day15 do
   def take_turn({{value, turn}, last_times}) do
-    spoken_when = Map.get(last_times, value)
-    if spoken_when do
-      {{turn - spoken_when, turn + 1}, Map.put(last_times, value, turn)}
-    else
-      {{0, turn + 1}, Map.put(last_times, value, turn)}
+    case Map.get(last_times, value) do
+      nil -> {{0, turn + 1}, Map.put(last_times, value, turn)}
+      spoken_when -> {{turn - spoken_when, turn + 1}, Map.put(last_times, value, turn)}
     end
   end
 
   def get_nth_turn(starter_nums, nth) do
     turns = Enum.with_index(starter_nums) |> Enum.reverse
-    last_times = for {x, i} <- turns, reduce: %{} do
-      acc -> Map.put(acc, x, i)
-    end
+    last_times = for {x, i} <- turns, into: %{}, do: {x, i}
     {{n, _}, _} = Stream.iterate({List.first(turns), last_times}, &take_turn/1)
     |> Enum.at(nth - Enum.count(turns))
     n
