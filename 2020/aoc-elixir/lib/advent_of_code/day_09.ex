fa9d4f60c375cf1d409de08a4664c604c456c4f4
Max Tyler <maxastyler@gmail.com>
2020-12-15 11:30:24 +0000

    Format nicer

--- 2020/aoc-elixir/lib/advent_of_code/day_09.ex
+++ 2020/aoc-elixir/lib/advent_of_code/day_09.ex
@@ -7,9 +7,16 @@ defmodule AdventOfCode.Day09 do
 
   def find_bad_encoding(nums) do
     Stream.chunk_every(nums, 26, 1)
-    |> Stream.reject(&(List.last(&1) in (for {x, i} <- Enum.with_index(Enum.take(&1, 25)),
-                                             y <- Enum.drop(&1, i) |> Enum.take(25), do: x + y)))
-    |> Enum.take(1) |> List.first |> List.last
+    |> Stream.reject(
+      &(List.last(&1) in for(
+          {x, i} <- Enum.with_index(Enum.take(&1, 25)),
+          y <- Enum.drop(&1, i) |> Enum.take(25),
+          do: x + y
+        ))
+    )
+    |> Enum.take(1)
+    |> List.first()
+    |> List.last()
   end
 
   def part1(args) do
@@ -20,9 +27,13 @@ defmodule AdventOfCode.Day09 do
   def part2(args) do
     input = parse_input(args)
     bad_num = find_bad_encoding(input)
-    Stream.map(2..Enum.count(input), &(Stream.chunk_every(input, &1, 1)))
-    |> Stream.concat
-    |> Stream.filter(&(Enum.sum(&1) == bad_num))
-    |> Enum.take(1) |> List.first |> (&(Enum.min(&1) + Enum.max(&1))).()
+
+    [bad_encoding | _] =
+      Stream.map(2..Enum.count(input), &Stream.chunk_every(input, &1, 1))
+      |> Stream.concat()
+      |> Stream.filter(&(Enum.sum(&1) == bad_num))
+      |> Enum.take(1)
+
+    Enum.min(bad_encoding) + Enum.max(bad_encoding)
   end
 end
