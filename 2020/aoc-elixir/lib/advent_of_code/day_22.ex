11f325ce8ecdc1ca1b7cfcfbd0555d107fcb7811
Max Tyler <maxastyler@gmail.com>
2020-12-22 12:51:09 +0000

    Finish day 22

--- 2020/aoc-elixir/lib/advent_of_code/day_22.ex
+++ 2020/aoc-elixir/lib/advent_of_code/day_22.ex
@@ -1,7 +1,63 @@
 defmodule AdventOfCode.Day22 do
+  def parse_input(input) do
+    [p1, p2] =
+      String.split(input, "\n\n")
+      |> Enum.map(fn x ->
+        String.split(x, "\n", trim: true) |> Enum.drop(1) |> Enum.map(&String.to_integer/1)
+      end)
+
+    {p1, p2}
+  end
+
+  def play_game_p1({[c1 | d1], [c2 | d2]}) do
+    cond do
+      c1 > c2 -> {d1 ++ [c1, c2], d2}
+      true -> {d1, d2 ++ [c2, c1]}
+    end
+    |> play_game_p1()
+  end
+
+  def play_game_p1({d1, d2}), do: d1 ++ d2
+
+  def play_round_p2({[c1 | d1], [c2 | d2]}) do
+    cond do
+      length(d1) >= c1 and length(d2) >= c2 ->
+        case play_game_p2({Enum.slice(d1, 0, c1), Enum.slice(d2, 0, c2)}) do
+          {:p1, _} -> {d1 ++ [c1, c2], d2}
+          {:p2, _} -> {d1, d2 ++ [c2, c1]}
+        end
+
+      c1 >= c2 ->
+        {d1 ++ [c1, c2], d2}
+
+      :else ->
+        {d1, d2 ++ [c2, c1]}
+    end
+  end
+
+  def play_game_p2({d1, d2} = state, history \\ MapSet.new()) do
+    cond do
+      state in history -> {:p1, d1 ++ d2}
+      length(d1) == 0 -> {:p2, d2}
+      length(d2) == 0 -> {:p1, d1}
+      true -> play_game_p2(play_round_p2(state), MapSet.put(history, state))
+    end
+  end
+
   def part1(args) do
+    for {v, i} <- play_game_p1(parse_input(args)) |> Enum.reverse() |> Enum.with_index(1),
+        reduce: 0 do
+      s -> s + v * i
+    end
   end
 
   def part2(args) do
+    for {v, i} <-
+          elem(play_game_p2(parse_input(args)), 1)
+          |> Enum.reverse()
+          |> Enum.with_index(1),
+        reduce: 0 do
+      s -> s + v * i
+    end
   end
 end
--- 2020/aoc-elixir/lib/mix/tasks/d22.p2.ex
+++ 2020/aoc-elixir/lib/mix/tasks/d22.p2.ex
@@ -5,7 +5,61 @@ defmodule Mix.Tasks.D22.P2 do
 
   @shortdoc "Day 22 Part 2"
   def run(args) do
-    input = nil
+    input = """
+    Player 1:
+    50
+    19
+    40
+    22
+    7
+    4
+    3
+    16
+    34
+    45
+    46
+    39
+    44
+    32
+    20
+    29
+    15
+    35
+    41
+    2
+    21
+    28
+    6
+    26
+    48
+
+    Player 2:
+    14
+    9
+    37
+    47
+    38
+    27
+    30
+    24
+    36
+    31
+    43
+    42
+    11
+    17
+    18
+    10
+    12
+    5
+    33
+    25
+    8
+    23
+    1
+    13
+    49
+    """
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
