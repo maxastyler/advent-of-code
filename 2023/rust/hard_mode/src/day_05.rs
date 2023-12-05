6a2e1bac59fc3445a779718a5ee2b5ccfd5d2ced
Max Tyler <maxastyler@gmail.com>
2023-12-05 08:04:01 +0000

    Start day 5c

---
+++ 2023/rust/hard_mode/src/day_05.rs
@@ -0,0 +1,99 @@
+use core::ops::Range;
+
+use crate::mem::Mem;
+
+/// Turn input into a pair of a str of numbers and a 7 element array of maps
+fn split_input<'a>(
+    input: &'a str,
+    mem: &'a mut Mem,
+) -> (&'a str, [&'a [(Range<usize>, Range<usize>)]; 7]) {
+    let (seeds, rest) = input.split_once("\n\n").unwrap();
+    let seeds = &seeds[7..];
+    let mut maps = rest.split("\n\n").map(|s| {
+        let s = s.trim_start_matches(|c: char| !c.is_ascii_digit());
+        let num_elem = s.lines().count();
+        let mut s_iter = s.lines();
+        mem.alloc_slice(s.lines().count(), |_| {
+            let mut line = s_iter
+                .next()
+                .unwrap()
+                .split_whitespace()
+                .map(|n| n.parse::<usize>().unwrap());
+            let dest = line.next().unwrap();
+            let start = line.next().unwrap();
+            let length = line.next().unwrap();
+            (start..(start + length), dest..(dest + length))
+        })
+        .unwrap()
+    });
+    (seeds, [(); 7].map(|_| &*maps.next().unwrap()))
+}
+
+fn translate(seed: usize, maps: &[&str]) -> usize {
+    let mut current_seed = seed;
+    for map in maps {
+        current_seed = translate_layer(current_seed, map);
+    }
+    current_seed
+}
+
+pub fn part_1(input: &str) -> usize {
+    let (seeds, maps) = split_input(input);
+    seeds
+        .split_whitespace()
+        .map(|x| x.parse::<usize>().unwrap())
+        .map(|seed| translate(seed, &maps))
+        .min()
+        .unwrap()
+}
+
+#[cfg(test)]
+mod test {
+    use crate::day_05::part_1;
+
+    use super::split_input;
+
+    const TEST_DATA: &str = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+
+    #[test]
+    fn split_input_works() {
+        // assert_eq!(split_input(TEST_DATA), ("", [""; 7]));
+    }
+
+    #[test]
+    fn part_1_works() {
+        assert_eq!(part_1(TEST_DATA), 35);
+    }
+}
--- 2023/rust/hard_mode/src/lib.rs
+++ 2023/rust/hard_mode/src/lib.rs
@@ -4,3 +4,4 @@ pub mod day_02;
 pub mod day_03;
 mod mem;
 pub mod day_04;
+pub mod day_05;
